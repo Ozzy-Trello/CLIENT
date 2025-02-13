21256989bef86296a61a316c58a0629da1186ca7
asaga22 <lharaskinanti@gmail.com>
2025-02-14 05:31:30 +0700

    setup react query

---
+++ src/app/provider/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "@/app/store";
+import { WorkspaceSidebarProvider } from "@/app/provider/workspace-sidebar-context";
+import { ThemeProvider } from "@/app/provider/theme-provider";
+import { ScreenSizeProvider } from "@/app/provider/screen-size-provider";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import QueryProvider from "./query-provider";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+        <QueryProvider>
+          <ScreenSizeProvider>
+            <AntdRegistry>
+              <ThemeProvider userId="1">
+                <WorkspaceSidebarProvider>
+                  {children}
+                </WorkspaceSidebarProvider>
+              </ThemeProvider>
+            </AntdRegistry>
+          </ScreenSizeProvider>
+        </QueryProvider>
+      </PersistGate>
+    </Provider>
+  );
+}
