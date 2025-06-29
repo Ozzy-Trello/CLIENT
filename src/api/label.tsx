e5ee1979937d32400976efb69bcd994fa30a0406
Ahmad Azzam <ahmadazzam123654@gmail.com>
2025-06-30 00:16:43 +0700

    fix: get all labels

--- src/api/label.tsx
+++ src/api/label.tsx
@@ -1,28 +1,46 @@
 import { api } from ".";
 import { ApiResponse } from "@myTypes/type";
 import { User } from "@dto/types";
-import { CardLabel, Label } from "@myTypes/label";
+import { CardLabel, Label, LabelAttributes } from "@myTypes/label";
 import snakecaseKeys from "snakecase-keys";
 
 export const createLabel = async (label: Label): Promise<ApiResponse<any>> => {
-  const {data} = await api.post(`/label`, label);
+  const { data } = await api.post(`/label`, label);
   return data;
-}
+};
 
-export const getLabels = async (workspaceId: string, params: CardLabel): Promise<ApiResponse<CardLabel[]>> => {
+export const getLabels = async (
+  workspaceId: string,
+  params: CardLabel
+): Promise<ApiResponse<CardLabel[]>> => {
   params = snakecaseKeys(params as any, { deep: true });
   const queryParams = new URLSearchParams(params as any).toString();
-  const {data} = await api.get(`/label?${queryParams}`, {headers: {"workspace-id": workspaceId}});
+  const { data } = await api.get(`/label?${queryParams}`, {
+    headers: { "workspace-id": workspaceId },
+  });
   return data;
-}
+};
 
-
-export const updateLabel = async (labelId: string, label: Label): Promise<ApiResponse<any>> => {
-  const {data} = await api.put(`/label/${labelId}`, label);
+export const updateLabel = async (
+  labelId: string,
+  label: Label
+): Promise<ApiResponse<any>> => {
+  const { data } = await api.put(`/label/${labelId}`, label);
   return data;
-}
+};
 
-export const deleteLabel = async (labelId: string): Promise<ApiResponse<any>> => {
-  const {data} = await api.delete(`/label/${labelId}`);
+export const deleteLabel = async (
+  labelId: string
+): Promise<ApiResponse<any>> => {
+  const { data } = await api.delete(`/label/${labelId}`);
   return data;
-}
+};
+
+export const getAllLabels = async (
+  workspaceId: string
+): Promise<ApiResponse<LabelAttributes[]>> => {
+  const { data } = await api.get(`/label/workspace`, {
+    headers: { "workspace-id": workspaceId },
+  });
+  return data;
+};
--- src/types/label.ts
+++ src/types/label.ts
@@ -2,11 +2,11 @@ export interface Label {
   id?: string;
   name?: string;
   value?: string;
-  valueType?: string; 
+  valueType?: string;
   workspaceId?: string;
   createdBy?: string;
   createdAt?: Date;
-  updatedAt?: Date
+  updatedAt?: Date;
 }
 
 export interface CardLabel {
@@ -15,10 +15,20 @@ export interface CardLabel {
   cardId?: string;
   name?: string;
   value?: string;
-  valueType?: string; 
+  valueType?: string;
   workspaceId?: string;
   isAssigned?: boolean;
   createdBy?: string;
   createdAt?: Date;
   updatedAt?: Date;
+}
+
+export interface LabelAttributes {
+  id: string;
+  name: string;
+  value?: string; // was color
+  workspace_id?: string; // was user_id
+  value_type: "color" | "user" | "custom_field"; // new enum field
+  created_at?: Date;
+  updated_at?: Date;
 }
