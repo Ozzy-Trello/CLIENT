8bd9142c6e9d80d65df2880189ced7c51e447eb7
Garry Agassi <agassi.garry@gmail.com>
2026-02-09 19:53:26 +0700

    feat: hide dashcard edit filters button for non super admin

--- src/components/modal-dashcard-detail/detail/filter/show/index.tsx
+++ src/components/modal-dashcard-detail/detail/filter/show/index.tsx
@@ -1,29 +1,39 @@
 import { FC } from "react";
+import { useSelector } from "react-redux";
 import { Button } from "antd";
 import { EditOutlined } from "@ant-design/icons";
 import { useCardDetailContext } from "@providers/card-detail-context";
+import { selectUser } from "@store/app_slice";
 import ItemFilter from "../item";
 
 const ShowFilter: FC = () => {
   const { dashcardConfig, setOpenEditFilter, setCurrentFilter } =
     useCardDetailContext();
+  const user = useSelector(selectUser);
+  const normalizedRole = (user?.role?.name || "").trim().toLowerCase();
+  const isSuperAdmin =
+    normalizedRole === "super admin" ||
+    normalizedRole === "super_admin" ||
+    normalizedRole === "superadmin";
   return (
     <div className="flex flex-col gap-3 w-full">
       {dashcardConfig?.filters?.map((filter) => (
         <ItemFilter key={filter.id} item={filter} />
       ))}
-      <Button
-        onClick={() => {
-          setCurrentFilter(
-            JSON.parse(JSON.stringify(dashcardConfig?.filters ?? []))
-          );
-          setOpenEditFilter(true);
-        }}
-        className="w-max"
-        icon={<EditOutlined />}
-      >
-        Edit Filters
-      </Button>
+      {isSuperAdmin && (
+        <Button
+          onClick={() => {
+            setCurrentFilter(
+              JSON.parse(JSON.stringify(dashcardConfig?.filters ?? []))
+            );
+            setOpenEditFilter(true);
+          }}
+          className="w-max"
+          icon={<EditOutlined />}
+        >
+          Edit Filters
+        </Button>
+      )}
     </div>
   );
 };
